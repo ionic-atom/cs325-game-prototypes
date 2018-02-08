905c9deab04949a152cfd6574513b0b655938474
ionic-atom <jdenton3@gmu.edu>
2018-02-08 12:07:46 -0500

    assign_one/

--- assign_one/js/main.js
+++ assign_one/js/main.js
@@ -18,16 +18,18 @@ window.onload = function() {
         game.load.image( 'background', 'assets/background_handmade.png');
 
         // Load Enemies
-        game.load.spritesheet( 'en_one', 'assets/spritesheet_idle.png', 100, 100, 48);
-        game.load.spritesheet( 'en_two', 'assets/spritesheet_idle.png', 100, 100, 48);
+        game.load.spritesheet( 'en_one', 'assets/spritesheet_rock1.png', 100, 100, 48);
+        game.load.spritesheet( 'en_two', 'assets/spritesheet_rock2.png', 100, 100, 48);
+        game.load.spritesheet( 'en_three', 'assets/spritesheet_rock3.png', 100, 100, 48);
     }
 
     // char
     var sprite;
 
     // Enemies
-    var enemy_one
-    var enemy_two
+    var enemy_one;
+    var enemy_two;
+    var enemy_three;
 
     // background
     var background;
@@ -56,18 +58,23 @@ window.onload = function() {
         // Enemies
         enemy_one = game.add.sprite(1200, 200, 'en_one');
         enemy_two = game.add.sprite(800, 400, 'en_two');
+        enemy_three = game.add.sprite(1600, 600, 'en_three');
 
+        var en_idle1 = enemy_one.animations.add('hover1');
+        var en_idle2 = enemy_two.animations.add('hover2');
+        var en_idle2 = enemy_three.animations.add('hover3');
 
-        var en_idle = enemy_one.animations.add('hover');
-        var en_idle = enemy_two.animations.add('hover');
-        enemy_one.animations.play('hover', 24, true);
-        enemy_two.animations.play('hover', 24, true);
+        enemy_one.animations.play('hover1', 24, true);
+        enemy_two.animations.play('hover2', 24, true);
+        enemy_three.animations.play('hover3', 24, true);
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( enemy_one, Phaser.Physics.ARCADE );
         game.physics.enable( enemy_two, Phaser.Physics.ARCADE );
+        game.physics.enable( enemy_three, Phaser.Physics.ARCADE );
         // Collision with world bounds
         enemy_one.body.collideWorldBounds = true;
         enemy_two.body.collideWorldBounds = true;
+        enemy_three.body.collideWorldBounds = true;
 
 
         // Key inputs for char
@@ -86,22 +93,26 @@ window.onload = function() {
             sprite.y -= 100;
             enemy_one.y += 100;
             enemy_two.y += 100;
+            enemy_three.y += 100;
         }
         else if (downKey.justDown){
             sprite.y += 100;
             enemy_one.y -= 100;
             enemy_two.y -= 100;
+            enemy_three.y -= 100;
         }
 
         if (leftKey.justDown){
             sprite.x -= 100;
             enemy_one.x += 100;
             enemy_two.x += 100;
+            enemy_three.x += 100;
         }
         else if (rightKey.justDown){
             sprite.x += 100;
             enemy_one.x -= 100;
             enemy_two.x -= 100;
+            enemy_three.x -= 100;
         }
 
     }
