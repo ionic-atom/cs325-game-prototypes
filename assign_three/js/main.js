c18f65d3510bac4648151cb457987cfe5acdd4a6
ionic-atom <jdenton3@gmu.edu>
2018-02-20 00:16:53 -0500

    assign_three/

--- assign_three/js/main.js
+++ assign_three/js/main.js
@@ -2,18 +2,85 @@
 
 window.onload = function() {
 
-	//	Create your Phaser game and inject it into the 'game' div.
-	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+    var game = new Phaser.Game( 800, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render} );
+    
+    function preload() {
+        // Loading images
+        game.load.image( 'char', 'assets/char.png' );
+        game.load.image( 'block', 'assets/char.png')
+    }
+    
+    var sprite;
+    var group;
 
-	//	Add the States your game has.
-	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
-	game.state.add('Boot', BasicGame.Boot);
-	game.state.add('Preloader', BasicGame.Preloader);
-	game.state.add('MainMenu', BasicGame.MainMenu);
-	game.state.add('Game', BasicGame.Game);
+    // Inventory/Items
+    var invent;
+    var dirt;
+    var grass;
+    var glass;
 
-	//	Now start the Boot state.
-	game.state.start('Boot');
+    var text;
 
+    
+    function create() {
+
+        game.world.setBounds(0, 0, 2400, 2400);
+        /*
+        for (var i = 0; i < 50; i++){
+            game.physics.enable(game.add.sprite(game.world.randomX, game.world.randomY, 'block'), Phaser.Physics.ARCADE);
+        }
+        */
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#0072bc';
+
+        sprite = game.add.sprite(50, 50, 'char');
+        sprite.anchor.setTo(0.5,0.5);
+        game.physics.enable(sprite, Phaser.Physics.ARCADE);
+        sprite.body.allowRotation = false;
+
+        group = game.add.physicsGroup();
+
+        for (var i = 0; i < 50; i++){
+            var c = group.create(game.rnd.between(400, 1000), game.rnd.between(600, 1200), 'block', game.rnd.between(0,12));
+        }
+
+
+        invent = new Array(dirt = 0, grass = 0, glass = 0);
+
+        text = game.add.text(sprite.position.x + 200, sprite.position.y + 200, "Inventory:\n" + "dirt: " + dirt + "\ngrass: " + grass, {font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"});
+
+    }
+    
+    function update() {
+
+        // Collision
+        if (game.physics.arcade.collide(sprite, group, collisionHandler, processHandler, this))
+        {
+            console.log('item_recieved');
+        }
+
+        sprite.rotation = game.physics.arcade.moveToPointer(sprite, 200, game.input.activePointer, 1400);
+        game.camera.x = sprite.body.x - 400;
+        game.camera.y = sprite.body.y - 400;
+    }
+
+    function render() {
+
+        game.debug.spriteInfo(sprite, 32, 32);
+    }
+
+    // Collision
+    function collisionHandler(player, item) {
+
+        item.kill();
+        dirt += 1;
+        text.setText("Inventory:\n" + "dirt: " + dirt + "\ngrass: " + grass);
+    }
+    // Collision
+    function processHandler(player, item) {
+
+        return true;
+    }
 };
